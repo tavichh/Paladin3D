ec86ad11a38d0d0d1c67df9025a2e55518b40f1e
tavichh <me@tavichh.com>
2025-11-24 03:27:58 -0500

    changed format and moved main

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-import instance;
-
-int main() {
-  graphics::vk_createInstance();
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+import instance;
+int main( ) {
+    graphics::vk_createInstance();
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
